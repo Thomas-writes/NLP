1069fb9a1c7d23aa02121b25ec4529cf65d4b239
Thomas Savasten <thomas@savasten.com>
2025-10-27 08:41:06 -0500

    new graphs/removing old

--- src/grapher.py
+++ src/grapher.py
@@ -128,29 +128,29 @@ def graph():
 
         return [avg_convex, avg_nonconvex, avg_fail, num_convex, num_nonconvex, num_fail]
 
-    #returns the list directly above
-    def graph_helper(l1, l2, l3, l4, l5, l6, l7, l8, title, filename):
-        avgL = avg_for(l1)
-        avgT = avg_for(l2)
-        avgP = avg_for(l3)
-        avgN = avg_for(l4)
-        avgC = avg_for(l5)
-        avgB = avg_for(l6)
-        avgD = avg_for(l7)
-        avgTN = avg_for(l8)
+    #given 4 lists a title, filename and a bounded flag make a graph
+    def graph_helper(l1, l2, l3, l4, title, filename, b_flag):
+        avg1 = avg_for(l1)
+        avg2 = avg_for(l2)
+        avg3 = avg_for(l3)
+        avg4 = avg_for(l4)
 
         #this sorts those results into useful information for the bars
-        methods = ["L-BFGS-B", "TNC", "Powell", "Nelder-Mead", "CG", "BFGS", "dogleg", "trust-ncg"]
-        convex_avgs = [avgL[0], avgT[0], avgP[0], avgN[0], avgC[0], avgB[0], avgD[0], avgTN[0]]
-        nonconvex_avgs = [avgL[1], avgT[1], avgP[1], avgN[1], avgC[1], avgB[1], avgD[1], avgTN[1]]
-        fail_avgs = [avgL[2], avgT[2], avgP[2], avgN[2], avgC[2], avgB[2], avgD[2], avgTN[2]]
-        total_convex = [avgL[3], avgT[3], avgP[3], avgN[3], avgC[3], avgB[3], avgD[3], avgTN[3]]
-        total_nonconvex = [avgL[4], avgT[4], avgP[4], avgN[4], avgC[4], avgB[4], avgD[4], avgTN[4]]
-        total_fail = [avgL[5], avgT[5], avgP[5], avgN[5], avgC[5], avgB[5], avgD[5], avgTN[5]]
+        if b_flag:
+            methods = ["L-BFGS-B", "TNC", "Powell", "Nelder-Mead"]
+        else:
+            methods = ["CG", "BFGS", "dogleg", "trust-ncg"]
+            
+        convex_avgs = [avg1[0], avg2[0], avg3[0], avg4[0]]
+        nonconvex_avgs = [avg1[1], avg2[1], avg3[1], avg4[1]]
+        fail_avgs = [avg1[2], avg2[2], avg3[2], avg4[2]]
+        total_convex = [avg1[3], avg2[3], avg3[3], avg4[3]]
+        total_nonconvex = [avg1[4], avg2[4], avg3[4], avg4[4]]
+        total_fail = [avg1[5], avg2[5], avg3[5], avg4[5]]
 
-        #x just is 8 evenly spaced __
-        x = np.arange(8)
-        width = 0.125
+        #x just is 4 evenly spaced __
+        x = np.arange(4)
+        width = 0.25
         #set the bar groups for each type of result
         bars1 = plt.bar(x - width, convex_avgs, width=width, label="Convex")
         bars2 = plt.bar(x, nonconvex_avgs, width=width, label="Nonconvex")
@@ -174,18 +174,18 @@ def graph():
         
     
     def graph_helper_2(l1, l2, l3, title, filename):
-        avgL = avg_for(l1)
-        avgT = avg_for(l2)
-        avgP = avg_for(l3)
+        avg1 = avg_for(l1)
+        avg2 = avg_for(l2)
+        avg3 = avg_for(l3)
 
         #this sorts those results into useful information for the bars
         methods = ["trust-constr", "SLSQP", "COBYLA"]
-        convex_avgs = [avgL[0], avgT[0], avgP[0]]
-        nonconvex_avgs = [avgL[1], avgT[1], avgP[1]]
-        fail_avgs = [avgL[2], avgT[2], avgP[2]]
-        total_convex = [avgL[3], avgT[3], avgP[3]]
-        total_nonconvex = [avgL[4], avgT[4], avgP[4]]
-        total_fail = [avgL[5], avgT[5], avgP[5]]
+        convex_avgs = [avg1[0], avg2[0], avg3[0]]
+        nonconvex_avgs = [avg1[1], avg2[1], avg3[1]]
+        fail_avgs = [avg1[2], avg2[2], avg3[2]]
+        total_convex = [avg1[3], avg2[3], avg3[3]]
+        total_nonconvex = [avg1[4], avg2[4], avg3[4]]
+        total_fail = [avg1[5], avg2[5], avg3[5]]
 
         #x just is 3 evenly spaced __
         x = np.arange(3)
@@ -211,7 +211,8 @@ def graph():
         
         
     
-    graph_helper(Llist, Tlist, Plist, Nlist, Clist, Blist, Dlist, TNlist, "All Type 1 methods all dimensions", "type1AllDims")
+    graph_helper(Llist, Tlist, Plist, Nlist, "All (bounded) Type 1 methods all dimensions", "bounded/type1AllDims", 1)
+    graph_helper(Clist, Blist, Dlist, TNlist, "All (unbounded) Type 1 methods all dimensions", "unbounded/type1AllDims", 0)
     graph_helper_2(TClist, SQlist, COlist, "All Type 2 methods all dimensions", "type2AllDims")
     
     l1, l2, l3 = [], [], []
@@ -315,10 +316,12 @@ def graph():
         else:
             l811.append(i)
     
-    graph_helper(l1, l2, l3, l4, l5, l6, l7, l8, "All Type 1 methods (dim <= 50)", "type1DimsSmall")
-    graph_helper(l11, l21, l31, l41, l51, l61, l71, l81, "All Type 1 methods (50 < dim <= 500)", "type1DimsMedium")
-    graph_helper(l111, l211, l311, l411, l511, l611, l711, l811, "All Type 1 methods (500 < dim)", "type1DimsLarge")
-    
+    graph_helper(l1, l2, l3, l4, "All (bounded) Type 1 methods (dim <= 50)", "bounded/type1DimsSmall", 1)
+    graph_helper(l5, l6, l7, l8, "All (unbounded) Type 1 methods (dim <= 50)", "unbounded/type1DimsSmall", 0)
+    graph_helper(l11, l21, l31, l41, "All (bounded) Type 1 methods (50 < dim <= 500)", "bounded/type1DimsMedium", 1)
+    graph_helper(l51, l61, l71, l81, "All (unbounded) Type 1 methods (50 < dim <= 500)", "unbounded/type1DimsMedium", 0)
+    graph_helper(l111, l211, l311, l411, "All (bounded) Type 1 methods (500 < dim)", "bounded/type1DimsLarge", 1)
+    graph_helper(l511, l611, l711, l811, "All (unbounded) Type 1 methods (500 < dim)", "unbounded/type1DimsLarge", 0)
 
     
 def main():
